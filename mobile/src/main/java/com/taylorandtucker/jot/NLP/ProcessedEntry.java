d735fd687c6f48a694671710fad2eab8442b906c
James Kirven <james.t.kirven@vanderbilt.edu>
2015-11-09 18:35:06 -0600

    graph no colors

--- mobile/src/main/java/com/taylorandtucker/jot/NLP/ProcessedEntry.java
+++ mobile/src/main/java/com/taylorandtucker/jot/NLP/ProcessedEntry.java
@@ -105,14 +105,9 @@ public class ProcessedEntry {
                 else if (sentSum < -2)
                     sentSum = -2;
             } else {
-                Pattern p = Pattern.compile("\\[\\[(.+)\\]\\]");
-                Matcher m = p.matcher(sentence);
 
-                if (m.find()) {
+                sentSum = 0;
 
-                    sentSum = Double.parseDouble(m.group(1));
-
-                }
             }
             sentiments.add(sentSum);
         }
